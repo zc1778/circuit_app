4157e3e1bbdc12184ffa086ac364b94d406ac0b5
Zachary Craddock <zcraddock1778@gmail.com>
2025-11-04 10:59:08 -0500

    Added initial login/register logic with firebase.

--- app/src/main/java/com/zybooks/circuitmaker/LoginActivity.java
+++ app/src/main/java/com/zybooks/circuitmaker/LoginActivity.java
@@ -1,41 +1,92 @@
 package com.zybooks.circuitmaker;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginActivity extends AppCompatActivity {
+
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
 
         Button loginButton = (Button) findViewById(R.id.loginButton);
         Button registerButton = (Button) findViewById(R.id.registerButton);
 
         loginButton.setOnClickListener( v -> {
-            // login logic
+            EditText emailInput = (EditText) findViewById(R.id.loginEmail);
+            EditText passwordInput = (EditText) findViewById(R.id.loginPassword);
+            loginCheck(emailInput.getText().toString(), passwordInput.getText().toString());
         });
 
         registerButton.setOnClickListener( v -> {
-            // register logic
+            EditText emailInput = (EditText) findViewById(R.id.loginEmail);
+            EditText passwordInput = (EditText) findViewById(R.id.loginPassword);
+            registerUser(emailInput.getText().toString(), passwordInput.getText().toString());
         });
     }
 
-    void loginCheck() {
-        EditText emailInput = (EditText) findViewById(R.id.loginEmail);
-        EditText passwordInput = (EditText) findViewById(R.id.loginPassword);
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            //idk
+        }
     }
 
-    void registerUser() {
-        EditText emailInput = (EditText) findViewById(R.id.loginEmail);
-        EditText passwordInput = (EditText) findViewById(R.id.loginPassword);
+    void loginCheck(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                        }
+                    }
+                });
+    }
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+    void registerUser(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                        }
+                    }
+                });
     }
 }
